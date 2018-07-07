b046c58b93c0f8c15d7026f137a5a14a770f43ac
pinklightsaber <ctorres@idemar.cl>
2018-07-07 14:26:01 -0400

    menus y bloginfo name

--- header.php
+++ header.php
@@ -9,19 +9,7 @@
 	<meta name="author" content="gettemplates.co" />
 	<?php get_template_part('_includes/iOS', 'icons') ?>
 
-	<!-- 
-	//////////////////////////////////////////////////////
 
-	FREE HTML5 TEMPLATE 
-	DESIGNED & DEVELOPED by FreeHTML5.co
-		
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
-
-	//////////////////////////////////////////////////////
-	 -->
 
   	<!-- Facebook and Twitter integration -->
 	<meta property="og:title" content=""/>
@@ -49,31 +37,11 @@
 					<div id="fh5co-logo"><a href="index.html">King.</a></div>
 				</div>
 				<div class="col-xs-10 text-right menu-1">
-					<ul>
-						<li class="active"><a href="index.html">Home</a></li>
-						<li><a href="work.html">Work</a></li>
-						<li><a href="about.html">About</a></li>
-						<li class="has-dropdown">
-							<a href="services.html">Services</a>
-							<ul class="dropdown">
-								<li><a href="#">Web Design</a></li>
-								<li><a href="#">eCommerce</a></li>
-								<li><a href="#">Branding</a></li>
-								<li><a href="#">API</a></li>
-							</ul>
-						</li>
-						<li class="has-dropdown">
-							<a href="#">Tools</a>
-							<ul class="dropdown">
-								<li><a href="#">HTML5</a></li>
-								<li><a href="#">CSS3</a></li>
-								<li><a href="#">Sass</a></li>
-								<li><a href="#">jQuery</a></li>
-							</ul>
-						</li>
-						<li><a href="contact.html">Contact</a></li>
-						<li class="btn-cta"><a href="#"><span>Login</span></a></li>
-					</ul>
+					<?php if(has_nav_menu('header_menu')) {  ?>
+						<?php wp_nav_menu(array(
+							'theme_location' => 'header-menu', 
+							'container'=> null)) ?>
+					<?php  } ?>
 				</div>
 			</div>
 			
