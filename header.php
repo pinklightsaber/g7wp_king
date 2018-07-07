676bcc0fec6524f57450e2ab757eb903a326d27f
pinklightsaber <ctorres@idemar.cl>
2018-07-06 22:09:54 -0400

    footer

--- header.php
+++ header.php
@@ -96,7 +96,7 @@
 		</div>
 	</nav>
 
-	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(<?php bloginfo('template_url') ?> /assets/images/img_bg_2.jpg);">
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(<?php bloginfo('template_url') ?>/assets/images/img_bg_2.jpg);">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
